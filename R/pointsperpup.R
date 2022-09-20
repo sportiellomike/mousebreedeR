bfeb8a4dcf1a7bf0c4d9c9462c5b987f65b8678b
Mike Sportiello <54905943+sportiellomike@users.noreply.github.com>
2022-09-20 12:22:17 -0400

    editing functions

--- R/pointsperpup.R
+++ R/pointsperpup.R
@@ -1,14 +1,10 @@
-#' fluximplied
+#' pointsperpup
 #'
-#' @description Pathway analysis of DESeq2 result or character vector of differentially expressed genes which also plots results.
-#' @param inputdat what you are using as your input data, either a dataframe with genes as the rownames, a column for LFC, and a column for padj values
-#' @param species either mus or hsa
-#' @param geneformat either ENTREZ or symbol
-#' @param inputformat either df or vector
-#' @param padjcolname the name of the column in your dataframe, if applicable, that stores the padj values
-#' @param pcutoff the alpha threshold for your padjustadjust
+#' @description Calculates how many points a potential pup would be worth according to its genotype.
+#' @param x summarizepotentialpupoutput
+#' @param desiredvector a vector of desired genotype.
 #'
-#' @return If a dataframe was supplied, it should also return a dataframe as well as a bar graph of the enriched pathways.
+#' @return returns the dataframe which now has points per pups.
 #' @export
 #'
 #' @examples
